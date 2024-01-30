fc0ee72a1ce3f36bcb8d6c3ae5609d1d55340394
farhana <farhana17mar@gmail.com>
2024-01-31 01:10:07 +0600

    done

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-console.log('hello world')
 
 
 function getComputerChoice () {
     let options = ['rock', 'paper', 'scissors'];
     let choice = options[Math.floor(Math.random()*options.length)]
-    console.log(choice)
+    console.log("computer's choice: ", choice) 
+    return choice;
 }
+
+
 
 function singleRound(playerSelection, computerSelection){
-    return "You Lose! Paper beats Rock"
-}
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-function playGame(){
-    for(let i = 0; i < 5; i++){
-        singleRound();
+    if( playerSelection == 'rock' && computerSelection == 'paper'){
+        return 'You lose because paper beats rock';
+    } else if( playerSelection == 'paper' && computerSelection == 'paper'){
+        return 'It is a tie';
+    } else if( playerSelection == 'scissors' && computerSelection == 'paper'){
+        return 'You win because scissors beats paper';
+    }
+    else if( playerSelection == 'scissors' && computerSelection == 'rock'){
+        return 'You lose because rock beats scissors';
+    }
+    else if( playerSelection == 'rock' && computerSelection == 'rock'){
+        return 'It is a tie';
+    }
+    else if( playerSelection == 'paper' && computerSelection == 'rock'){
+        return 'You lost because rock beats paper';
+    }
+    else if( playerSelection == 'scissors' && computerSelection == 'scissors'){
+        return 'it is a tie';
+    }
+    else if( playerSelection == 'rock' && computerSelection == 'scissors'){
+        return 'You win because scissors beats rock';
+    }
+    else if( playerSelection == 'paper' && computerSelection == 'scissors'){
+        return 'You lose because scissors beats paper';
+    } else {
+        return 'please input a valid option'
     }
 }
-let userInput = prompt("What's your choice among rock, paper, scissors");
-getComputerChoice();
+
+// const playerSelection = "rock";
+
+
+
+function playGame(){
+     for(let i = 0; i < 5; i++){
+        let playerSelection = prompt("What's your choice among rock, paper, scissors");
+        const computerSelection = getComputerChoice();
+        console.log(singleRound(playerSelection, computerSelection));
+
+     }
+}
+
+playGame();
